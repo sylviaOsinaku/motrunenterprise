c2de13bed127e3e1719a12f3913a972731160337
sylviaOsinaku <osinakusylvia@gmail.com>
2026-04-06 19:40:23 +0100

    changed logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { Menu, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import logo from '../public/images/motunlogo.jpg'
+import logo from "../public/motrun-main-logo.png"
 import Image from 'next/image'
 export function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
